77114d3116c1ac30cb49140dd073319ec4d9ecfe
luan-nee <luandelsol54@gmail.com>
2024-07-20 17:08:06 -0500

    primer commit

---
+++ src/main/java/model/administradordetareas/AdministradorDeTareas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package model.administradordetareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AdministradorDeTareas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
