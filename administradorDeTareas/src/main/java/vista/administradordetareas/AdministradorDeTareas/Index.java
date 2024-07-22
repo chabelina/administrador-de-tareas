a647e0034f378541562938d2abfcc05e819b302d
luan-nee <luandelsol54@gmail.com>
2024-07-22 13:44:16 -0500

    agregando el paquete principal

---
+++ administradorDeTareas/src/main/java/vista/administradordetareas/AdministradorDeTareas/Index.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista.administradordetareas.AdministradorDeTareas;
+
+import vista.Login;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Index {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Login principal = new Login();
+        principal.setVisible(true);
+    }
+}
