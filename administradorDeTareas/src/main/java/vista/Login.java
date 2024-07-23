de946548df4a03eadd8c4c5f46b25e2592a4beaf
yukio <zviamontevilca@gmail.com>
2024-07-22 21:09:01 -0500

    cambios
    agregando correciones de base de datos y agrupaciones

--- administradorDeTareas/src/main/java/vista/Login.java
+++ administradorDeTareas/src/main/java/vista/Login.java
@@ -10,6 +10,7 @@ public class Login extends javax.swing.JFrame {
    */
   public Login() {
     initComponents();
+    this.setExtendedState(this.MAXIMIZED_BOTH);
     //this.agregarIconoPanel(pnlLogo, "img/logo.png");
   }
   
