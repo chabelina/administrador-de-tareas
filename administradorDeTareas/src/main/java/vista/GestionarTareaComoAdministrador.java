06741987e290f9c1a00610e9585c629bd121b3a7
luan-nee <luandelsol54@gmail.com>
2024-07-24 18:19:03 -0500

    agregando la interfaz de "GESTIONAR TAREA COMO ADMINISTRADOR"

--- administradorDeTareas/src/main/java/vista/GestionarTareaComoAdministrador.java
+++ administradorDeTareas/src/main/java/vista/GestionarTareaComoAdministrador.java
@@ -4,6 +4,12 @@
  */
 package vista;
 
+import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+import EstilosComponents.GestionEncabezadoTabla;
+
 /**
  *
  * @author Usuario
@@ -13,8 +19,27 @@ public class GestionarTareaComoAdministrador extends javax.swing.JFrame {
   /**
    * Creates new form GestionarTareaComoAdministrador
    */
+  DefaultTableModel modelo;
+  
+  
   public GestionarTareaComoAdministrador() {
     initComponents();
+    
+    modelo = new DefaultTableModel();
+    modelo.addColumn("ID");
+    modelo.addColumn("Nombre de la Tabla");
+    modelo.addColumn("Descripción");
+    modelo.addColumn("Estado");
+    modelo.addColumn("Prioridad");
+    modelo.addColumn("Fecha de Asignación");
+    modelo.addColumn("Fecha Limite");
+    modelo.addColumn("Responsable");
+    this.table_tareas.setModel(modelo);
+  
+    JTableHeader theader = table_tareas.getTableHeader();
+    theader.setDefaultRenderer(new GestionEncabezadoTabla());
+    //this.table_tareas.setBackground(Color.HSBtoRGB(225,225,225));
+    table_tareas.setBackground(Color.red);
   }
 
   /**
@@ -26,21 +51,201 @@ public class GestionarTareaComoAdministrador extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
+    jPanel1 = new javax.swing.JPanel();
+    jPanel3 = new javax.swing.JPanel();
+    jLabel4 = new javax.swing.JLabel();
+    jLabel6 = new javax.swing.JLabel();
+    jButton1 = new javax.swing.JButton();
+    jButton2 = new javax.swing.JButton();
+    jButton3 = new javax.swing.JButton();
+    jScrollPane1 = new javax.swing.JScrollPane();
+    table_tareas = new javax.swing.JTable();
+    jPanel2 = new javax.swing.JPanel();
+    jLabel1 = new javax.swing.JLabel();
+    jLabel2 = new javax.swing.JLabel();
+
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+    jPanel1.setBackground(new java.awt.Color(19, 30, 35));
+
+    jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+    jPanel3.addMouseListener(new java.awt.event.MouseAdapter() {
+      public void mouseClicked(java.awt.event.MouseEvent evt) {
+        jPanel3MouseClicked(evt);
+      }
+    });
+
+    jLabel4.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+    jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+    jLabel4.setText("REGRESAR");
+
+    jLabel6.setText("icono");
+
+    javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+    jPanel3.setLayout(jPanel3Layout);
+    jPanel3Layout.setHorizontalGroup(
+      jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+    );
+    jPanel3Layout.setVerticalGroup(
+      jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
+        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+    );
+
+    jButton1.setBackground(new java.awt.Color(206, 95, 14));
+    jButton1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+    jButton1.setForeground(new java.awt.Color(255, 255, 255));
+    jButton1.setText("VISUALIZAR A TODOS LOS MIEMBROS");
+    jButton1.setBorder(null);
+    jButton1.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jButton1ActionPerformed(evt);
+      }
+    });
+
+    jButton2.setBackground(new java.awt.Color(0, 140, 219));
+    jButton2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+    jButton2.setForeground(new java.awt.Color(255, 255, 255));
+    jButton2.setText("MAODIFICAR TAREA ASIGNADA");
+    jButton2.setBorder(null);
+    jButton2.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jButton2ActionPerformed(evt);
+      }
+    });
+
+    jButton3.setBackground(new java.awt.Color(8, 131, 20));
+    jButton3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+    jButton3.setForeground(new java.awt.Color(255, 255, 255));
+    jButton3.setText("ASIGNAR NUEVA TAREA");
+    jButton3.setBorder(null);
+    jButton3.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jButton3ActionPerformed(evt);
+      }
+    });
+
+    table_tareas.setBackground(new java.awt.Color(19, 30, 35));
+    table_tareas.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+    table_tareas.setForeground(new java.awt.Color(255, 255, 255));
+    table_tareas.setModel(new javax.swing.table.DefaultTableModel(
+      new Object [][] {
+        {null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null},
+        {null, null, null, null, null, null, null}
+      },
+      new String [] {
+        "ID", "Nombre de la Tarea", "Descripción ", "Estado", "Prioridad", "Fecha Limite", "Responsable"
+      }
+    ));
+    table_tareas.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+    jScrollPane1.setViewportView(table_tareas);
+    if (table_tareas.getColumnModel().getColumnCount() > 0) {
+      table_tareas.getColumnModel().getColumn(0).setHeaderValue("ID");
+      table_tareas.getColumnModel().getColumn(1).setHeaderValue("Nombre de la Tarea");
+      table_tareas.getColumnModel().getColumn(2).setHeaderValue("Descripción ");
+      table_tareas.getColumnModel().getColumn(3).setHeaderValue("Estado");
+      table_tareas.getColumnModel().getColumn(4).setHeaderValue("Prioridad");
+      table_tareas.getColumnModel().getColumn(5).setHeaderValue("Fecha Limite");
+      table_tareas.getColumnModel().getColumn(6).setHeaderValue("Responsable");
+    }
+
+    jPanel2.setBackground(new java.awt.Color(19, 30, 35));
+
+    jLabel1.setFont(new java.awt.Font("Arial", 1, 36)); // NOI18N
+    jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+    jLabel1.setText("NOMBRE DEL PROYECTO");
+
+    jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+    jLabel2.setText("Aquí va la descripción del proyecto, sobre que trata, para que sirve, etc...");
+    jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+
+    javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+    jPanel2.setLayout(jPanel2Layout);
+    jPanel2Layout.setHorizontalGroup(
+      jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+      .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+    );
+    jPanel2Layout.setVerticalGroup(
+      jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel2Layout.createSequentialGroup()
+        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE)
+        .addGap(0, 0, 0))
+    );
+
+    javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+    jPanel1.setLayout(jPanel1Layout);
+    jPanel1Layout.setHorizontalGroup(
+      jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+        .addContainerGap(79, Short.MAX_VALUE)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+          .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addGroup(jPanel1Layout.createSequentialGroup()
+            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(67, 67, 67)
+            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 351, javax.swing.GroupLayout.PREFERRED_SIZE))
+          .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING))
+        .addGap(23, 23, 23))
+    );
+    jPanel1Layout.setVerticalGroup(
+      jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addGroup(jPanel1Layout.createSequentialGroup()
+        .addGap(34, 34, 34)
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+          .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+          .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        .addGap(18, 18, 18)
+        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 407, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addContainerGap(56, Short.MAX_VALUE))
+    );
 
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 1333, Short.MAX_VALUE)
+      .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
     );
     layout.setVerticalGroup(
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 666, Short.MAX_VALUE)
+      .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
     );
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
+
+  private void jPanel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MouseClicked
+    
+  }//GEN-LAST:event_jPanel3MouseClicked
+
+  private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_jButton1ActionPerformed
+
+  private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_jButton2ActionPerformed
+
+  private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    // TODO add your handling code here:
+  }//GEN-LAST:event_jButton3ActionPerformed
 
   /**
    * @param args the command line arguments
@@ -78,5 +283,17 @@ public class GestionarTareaComoAdministrador extends javax.swing.JFrame {
   }
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
+  private javax.swing.JButton jButton1;
+  private javax.swing.JButton jButton2;
+  private javax.swing.JButton jButton3;
+  private javax.swing.JLabel jLabel1;
+  private javax.swing.JLabel jLabel2;
+  private javax.swing.JLabel jLabel4;
+  private javax.swing.JLabel jLabel6;
+  private javax.swing.JPanel jPanel1;
+  private javax.swing.JPanel jPanel2;
+  private javax.swing.JPanel jPanel3;
+  private javax.swing.JScrollPane jScrollPane1;
+  private javax.swing.JTable table_tareas;
   // End of variables declaration//GEN-END:variables
 }
