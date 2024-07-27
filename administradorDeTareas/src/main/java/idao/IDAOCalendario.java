de91515156926046dcadae98d1e5788b4233a132
luan-nee <luandelsol54@gmail.com>
2024-07-27 11:15:54 -0500

    cambiando de color las letras de los botónes, de los que estaban negro a blanco para mejor contraste

---
+++ administradorDeTareas/src/main/java/idao/IDAOCalendario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package idao;
+
+import java.util.List;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface IDAOCalendario {
+  List<String[]> obtenerDiasMes(String mes);
+}
